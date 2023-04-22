78b76b7eb22cf6a54e08b8b0009e7c9a1ef7f3e3
ppolindrom <ipollyce@mail.ru>
2023-04-22 14:49:53 +0500

    add new str_func

--- str_func.py
+++ str_func.py
@@ -1,2 +1,11 @@
 def user_input(string):
+    """
+    увеличивает все буквы
+    """
+    return string.upper
+
+def user_input(string):
+    """
+    увеличивает первую букву
+    """
     return string.upper
